2ac292eec48ce4fc406d9746c12b1f389f9f4aa7
Otakoe <vitalylarc@gmail.com>
2020-04-03 18:43:52 +0300

    Приступил к заданию №2

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 import java.util.concurrent.*;
 
@@ -28,39 +30,49 @@ public class Exercise1 {
             operator=scan.next();
             switch (operator){
                 case "+":
+                    whatThread();
                     System.out.println("Сумма двух чисел равна "+plus(serv,a,b));
                     break;
                 case "-":
+                    whatThread();
                     System.out.println("Разность двух чисел равна "+minus(serv,a,b));
                     break;
                 case "*":
+                    whatThread();
                     System.out.println("Произведение двух чисел равно "+mult(serv,a,b));
                     break;
                 case "/":
+                    whatThread();
                     System.out.println("Частное двух чисел равно "+div(serv,a,b));
                     break;
                 case "%":
+                    whatThread();
                     System.out.println("Остаток от деления А на Б равен "+divLeft(serv,a,b));
                     break;
                 case "==":
+                    whatThread();
                     if(isEqual(serv,a,b))
                         System.out.println("Числа равны");
                     else
                         System.out.println("Числа не равны");
                     break;
                 case "<":
+                    whatThread();
                     if(less(serv,a,b))
                         System.out.println("Да, А меньше Б");
                     else
                         System.out.println("Нет, А не меньше Б");
                     break;
                 case ">":
+                    whatThread();
                     if(more(serv,a,b))
                         System.out.println("Да, А больше Б");
                     else
                         System.out.println("Нет, А не больше Б");
                     break;
                 case "0":
+                    whatThread();
+                    System.out.println("До свидания");
                     break exitWhile;
                 default:
                     System.out.println("Неверный ввод, для выхода нажмите 0");
@@ -109,5 +121,9 @@ public class Exercise1 {
     static boolean less(ExecutorService serv,int a, int b) throws ExecutionException, InterruptedException {
         Future<Boolean> future= serv.submit(() -> a<b);
         return future.get();
+    }
+
+    static void whatThread(){
+        System.out.print(Thread.currentThread().getName()+" поток говорит: ");
     }
 }
