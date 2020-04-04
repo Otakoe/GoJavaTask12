02cefab48673cbd3e6b532070352851463fe6fe0
Otakoe <vitalylarc@gmail.com>
2020-04-04 21:34:33 +0300

    Выполнил задание №2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,13 +1,7 @@
-import org.w3c.dom.ls.LSOutput;
-
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Exercise2 {
     /*Написать функцию которая создает массив интов размером size. Числа в массиве идут по возрастанию от 1 до size.
@@ -26,78 +20,102 @@ public class Exercise2 {
 Режим Thread и Thread Pool не могут работать одновременно друг с другом. Только по очереди.
 */
     public static void main(String[] args) throws InterruptedException {
-        //оптимальное число потоков после которого для этой задачи прирост в производительности незначителен, или наоборот хуже и ошибки в вещественном числе растут.
-        int numOfThreads = 16;
+        //оптимальное число потоков для режима Сред =16, после прирост в производительности незначителен, или наоборот хуже и ошибки в вещественном числе растут.
+        int numOfThreads = 8;
+        int divArray =16;
         int arr[] = createArray(80000000);
-        int N;
+        int N=0;
         Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число сколько раз подряд провести подсчёт");
         try {
             N = scan.nextInt();
             ThreadMode(arr, N, numOfThreads);
-            //ThreadPoolMode(arr,N,numOfThreads);
+            ThreadPoolMode(arr,N,numOfThreads,divArray);
         }catch (Exception e){
             System.out.println("Wrong input");
         }
     }
 
-    static void ThreadPoolMode (int arr[],int N,int numOfThreads){
-        ExecutorService serv1 = Executors.newFixedThreadPool(N);
-        FutureTask<Double> future = new FutureTask<Double>(()->{
-            double result=0.0;
-
-           return result;
-        });
-
-
-
-
+    static void ThreadPoolMode (int arr[],int N,int numOfThreads,int divArray) throws ExecutionException, InterruptedException {
+        ExecutorService service1 = Executors.newFixedThreadPool(numOfThreads+1);
+        Callable<Double> callableSum = () -> {
+            Double sum = 0.0;
+            ArrayList<Future<Double>> futures = new ArrayList<>();
+        //цикл определяет и разбивает выполнение функции по потокам, каждому свой интервал массива
+        //проходимся по этому циклу создавая список будующего субмитом калабла в виде лямбды
+                for (int i = 0; i < arr.length; i += arr.length / divArray) {
+                    int fi = i;
+                    futures.add(service1.submit(()->{
+                        return calcArray(arr, fi, fi + arr.length / divArray);
+                    }));
+                }
+        //собираем значения будующего по мере его готовности и суммируем
+                for (Future<Double> future:futures)
+                {
+                    sum+=future.get();
+                }
+                return sum;
+        };
+        //поочерёдный вызов потоков
+        long timeOnStart = System.currentTimeMillis();
         for (int i =0;i<N;i++){
-            serv1.submit(future);
+            System.out.println("Итерация № "+(i+1));
+            System.out.println("сумма результатов = " + service1.submit(callableSum).get());
         }
+        System.out.println("Программа в режиме threadPool затратила = " + (System.currentTimeMillis() - timeOnStart)/1000.0+" сукунд");
+        service1.shutdown();
     }
-
+    //функция работает с массивом в указанном интервале
+    static double calcArray(int arr[], int iBegin, int iEnd) {
+        double result= 0.0;
+        for (int i = iBegin;i<iEnd;i++){
+            result+=calc(arr[i]);
+        }
+        return result;
+    }
 
     static void ThreadMode(int arr[], int N,int numOfThreads) throws InterruptedException {
         long timeOnStart = System.currentTimeMillis();
-        Runnable run = () -> {
-            AtomicDouble sum = new AtomicDouble();
+        Runnable runnable = () -> {
+            AtomicDouble sum = new AtomicDouble(0.0);
             int normalThreadCount = Thread.activeCount(); //нужно для костыля
-            System.out.println(Thread.activeCount());
             //создаём фиксированное количество потоков
-            for (int t = 0; t < numOfThreads; t++) {
-                int tt = t;
+            //каждому потоку пропорционально свой кусок массива
+            for (int i = 0; i < arr.length; i+=arr.length/numOfThreads) {
+                int fi = i;
                 Thread threadInside = new Thread(() -> {
-                    //каждому потоку пропорционально свой кусок массива
-                    double sumInThread = 0;
-                            //System.out.println(arr.length/numOfThreads*tt+" do "+arr.length/numOfThreads*(tt+1)); //для дебага, показывает какой отрезок массива какой поток получил
-                    for (int i = arr.length / numOfThreads * tt; i < arr.length / numOfThreads * (tt + 1); i++) {
-                        int it = i;
-                        sumInThread += calc(it);
-                    }
-                            //System.out.print(Thread.currentThread().getName()+" "+sum.get()+ " " +sumInThread+" "); //для дебага, показывает сколько было в атомарном дубле и сколько насчитал поток.
-                    sum.set(sum.get() + sumInThread);
+                    double sumInThread = calcArray(arr,fi,fi+arr.length/numOfThreads);
+                    //System.out.println(fi+" do "+(fi +arr.length/numOfThreads)); //для дебага, показывает какой отрезок массива какой поток получил
+                    sum.add(sumInThread);
                 });
+
                 threadInside.start();
             }
             //костыль, чтобы вся программа подождала завершение потоков в форе
             while (Thread.activeCount() > normalThreadCount) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
             System.out.println("сумма результатов = " + sum.get());
         };
 
         for (int i=0;i<N;i++) {
-            Thread threadN = new Thread(run);
-            System.out.println("Итерация № "+i);
+            Thread threadN = new Thread(runnable);
+            System.out.println("Итерация № "+(i+1));
             threadN.start();
             threadN.join();
         }
         System.out.println("Программа в режиме thread затратила = " + (System.currentTimeMillis() - timeOnStart)/1000.0+" сукунд");
     }
-//операция одного вычисление (на 1 000 000 итераций 78мс)
+
+//операция одного вычисление (на 1 000 000 итераций ~78мс)
     static double calc ( double x){
             return Math.sin(x) + Math.cos(x);
         }
-//создание большого массива (172мс)
+//создание большого массива (~172мс)
     static int[] createArray ( int size){
             int arr[] = new int[size];
             for (int i = 0; i < arr.length; i++) {
@@ -129,6 +147,9 @@ public class Exercise2 {
                 return Double.longBitsToDouble(bits.getAndSet(Double.doubleToLongBits(newValue)));
             }
 
+            public final void add(double value){
+                set(get()+ value);
+            }
             @Override
             public int intValue() {
                 return (int) get();
